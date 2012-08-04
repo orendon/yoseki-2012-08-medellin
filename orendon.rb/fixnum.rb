92c313ed2c3ac588cf6459f026717a6b70d6ce3f
Oscar Rendon <oarendon@gmail.com>
2012-08-03 20:33:05 -0500

    add example, and group base roman numerals

--- orendon.rb/fixnum.rb
+++ orendon.rb/fixnum.rb
@@ -1,5 +1,11 @@
 class Fixnum
+  @@roman_numerals = {
+    1 => "I",   2 => "II",    3 => "III",   4 => "IV",
+    5 => "V",   6 => "VI",    7 => "VII",   8 => "VIII",
+    9 => "IX",  10=> "X"
+  }
+
   def in_roman
-    "I"*self
+   @@roman_numerals[self]
   end
 end
--- orendon.rb/spec/roman_numbers_spec.rb
+++ orendon.rb/spec/roman_numbers_spec.rb
@@ -11,4 +11,12 @@ describe "arabic to roman conversions" do
     2.in_roman.should be_eql "II"
   end
 
+  it "should convert 3 into III" do
+    3.in_roman.should be_eql "III"
+  end
+
+  it "should convert 8 into VIII" do
+    8.in_roman.should eq "VIII"
+  end
+
 end
